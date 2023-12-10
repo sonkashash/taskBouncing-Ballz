4235e7beee4cded33024c59ccfd10794e5cfb343
Sofia <sophia.shashova@yandex.ru>
2023-12-10 11:50:37 +0400

    jump x and y coordinates

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// const ball = document.getElementById("ball");
-// let x = 0;
-//     let y = 0;
-//     let xSpeed = 5;
-//     let ySpeed = 5;
-//     function animate() {
-//     x += xSpeed;
-//     y += ySpeed;
-//     if (x + 50 > window.innerWidth || x < 0) {
-//        xSpeed = -xSpeed;
-//     }
-//     if (y + 50 > window.innerHeight || y < 0) {
-//        ySpeed = -ySpeed;
-//     }
-//     ball.style.left = x + "px";
-//     ball.style.top = y + "px";
-//     requestAnimationFrame(animate);
-//  }
-// animate();
 // document.onmousemove = (e) => {
 //     document.onclick = () => {
 //         let ball = document.createElement('ball');
@@ -24,37 +5,74 @@
 //         ball.style.left = e.pageX + 'px';
 //         ball.style.top = e.pageY + 'px';
 //         document.body.appendChild(ball)
-//     }   
+//     }
 // }
-const ball    = document.querySelector('.ball');
-let y_coord = 1;
-let dy_coord = 1;
+// function draw() {
+//     background(0, 200, 0)
+// }
 
-const gravity = () => {
-    myReq = requestAnimationFrame(gravity)
-    if(y_coord > 460) {
-        dy_coord = -dy_coord;
-    } else {
-        dy_coord -= 1
-    }
-    dy_coord += 2;
-    if(y_coord > 465){
-        cancelAnimationFrame(myReq);
-    }
-    if(y_coord === 420){
-        console.log('dy_coord is ', dy_coord)
-    }
-    console.log(dy_coord);
-    y_coord += dy_coord;
-    ball.style.top = (y_coord) + "px";
-}
+// const ball    = document.querySelector('.ball');
+// let y_coord = 1;
+// let dy_coord = 1;
 
-gravity();
+// const gravity = () => {
+//     myReq = requestAnimationFrame(gravity)
+//     if(y_coord > 460) {
+//         dy_coord = -dy_coord;
+//     } else {
+//         dy_coord -= 1
+//     }
+//     dy_coord += 2;
+//     if(y_coord > 465){
+//         cancelAnimationFrame(myReq);
+//     }
+//     if(y_coord === 420){
+//         console.log('dy_coord is ', dy_coord)
+//     }
+//     console.log(dy_coord);
+//     y_coord += dy_coord;
+//     ball.style.top = (y_coord) + "px";
+// }
+
+// gravity();
 // setInterval(function(){
 
-//   if(y_coord>425) {dy_coord = -dy_coord;} 
+//   if(y_coord>425) {dy_coord = -dy_coord;}
 //   dy_coord+=1;
 //   y_coord = y_coord + dy_coord;
 //   ball.style.top = (y_coord) + "px";
 // },40);
+var example = document.getElementById("example"),
+ctx = example.getContext("2d");
+ctx.fillRect(0, 0, example.width, example.height);
 
+const ball = document.querySelector(".ball");
+let x_coord = 1;
+let dx_coord = 1;
+let y_coord = 1;
+let dy_coord = 1;
+const gravity = 0.5;
+
+const bounce = () => {
+  myReq = requestAnimationFrame(bounce);
+  dy_coord += gravity;
+  y_coord += dy_coord;
+  x_coord += dx_coord;
+  if (x_coord > 270 || x_coord<1 ) {
+    dx_coord *= -1;
+  }
+  if (y_coord > 450) {
+    dy_coord *= -1;
+  }
+  ball.style.top = y_coord + "px";
+  ball.style.left = x_coord + "px";
+  if (y_coord > 451) {
+    cancelAnimationFrame(myReq);
+  }
+  if (mouseIsPressed) {
+    y_coord = mouseY;
+    x_coord = mouseX;
+  }
+};
+
+bounce();
