bd96c2963246899afc3d60eddc3b5ae46e637c14
Sofia <sophia.shashova@yandex.ru>
2023-12-14 13:55:32 +0400

    code formatted

--- script.js
+++ script.js
@@ -1,42 +1,34 @@
-const ballArray = [];
-const gravity = 1;
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+const ballArray = [];
+const gravity = 0.9;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-//изменения размеров при сужении окна
 window.addEventListener("resize", () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 });
-//
 
-//обработка клика
 const mouse = {
   x: undefined,
   y: undefined,
 };
-//
-let speedX = Math.random() * 2;
-let speedY = 1;
+
 canvas.addEventListener("click", (e) => {
   mouse.x = e.x;
   mouse.y = e.y;
-  ballArray.push(new Ball(speedX, speedY, mouse.y));
+  ballArray.push(new Ball());
 });
 
-let y = mouse.y;
-
-//класс Ball
 class Ball {
-  constructor(speedX, speedY, y) {
+  constructor() {
     this.x = mouse.x;
-    this.y = y;
+    this.y = mouse.y;
     this.size = Math.random() * 50;
     this.speedX = Math.random() * 2;
-    this.speedY = speedY;
+    this.speedY = 1;
   }
   update() {
     this.draw;
@@ -54,14 +46,13 @@ class Ball {
       this.y + this.speedY > canvas.height - this.size ||
       this.y + this.speedY < this.size
     ) {
-      this.speedY *= -0.99;
+      this.speedY *= -0.9;
     }
     if (this.y > canvas.height - this.size) {
       this.y = canvas.height - this.size;
       this.speedY = 0;
       this.speedX = 0;
     }
-    return speedY;
   }
 
   draw() {
@@ -74,16 +65,13 @@ class Ball {
     ctx.closePath();
   }
 }
-//конец класс Ball
 
-//отрисовка компонентов и падение мяча
 function handle() {
   for (i = 0; i < ballArray.length; i++) {
     ballArray[i].draw();
     ballArray[i].update();
   }
 }
-//
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -92,10 +80,8 @@ function animate() {
   //   if (ballArray.length>=1 && JSON.parse((ballArray[ballArray.length-1].speedY))==0 ) {
   //   cancelAnimationFrame(myReq)
   //  }
-  //   }, 15000)
+  //   }, 10000)
   myReq = requestAnimationFrame(animate);
-  // if (ballArray.length>=1 && JSON.parse((ballArray[ballArray.length-1].speedY))==0) {
-  //   cancelAnimationFrame(myReq) }
 }
 
 animate();
